a9b7d27d5a117c84802b2b0f355eb19875cde09d
olbender <olbender@anonomous.github.com>
2019-07-14 23:36:16 +0200

    First R

--- src/opendlv-perception-detect-yolo.cpp
+++ src/opendlv-perception-detect-yolo.cpp
@@ -21,12 +21,14 @@
 #include <memory>
 #include <string>
 
+#include <X11/Xlib.h>
+
 #include <yolo_v2_class.hpp>
 
 #include "cluon-complete.hpp"
 #include "opendlv-standard-message-set.hpp"
 
-static uint8_t getPixelExtendArgb(char *img, uint32_t w, uint32_t h, int32_t x, 
+static uint8_t getPixelExtendArgb(char *img, uint32_t w, uint32_t h, int32_t x,
     int32_t y, uint32_t c)
 {
     if (x < 0 || x >= static_cast<int32_t>(w) || y < 0 
@@ -45,26 +47,58 @@ static float bilinearInterpolationArgb(char *img, uint32_t w, uint32_t h,
   float dx = x - ix;
   float dy = y - iy;
 
-  float p1 = (1 - dy) * (1 - dx) * getPixelExtendArgb(img, w, h, ix, iy, c);
-  float p2 = dy * (1 - dx) * getPixelExtendArgb(img, w, h, ix, iy + 1, c);
-  float p3 = (1 - dy) * dx * getPixelExtendArgb(img, w, h, ix + 1, iy, c);
-  float p4 = dy * dx * getPixelExtendArgb(img, w, h, ix + 1, iy + 1, c);
-
-  return p1 + p2 + p3 + p4;
-  //return getPixelExtendArgb(img, w, h, ix, iy, c);
+  return (1 - dy) * (1 - dx) * getPixelExtendArgb(img, w, h, ix, iy, c) 
+    + dy * (1 - dx) * getPixelExtendArgb(img, w, h, ix, iy + 1, c) 
+    + (1 - dy) * dx * getPixelExtendArgb(img, w, h, ix + 1, iy, c) 
+    + dy * dx * getPixelExtendArgb(img, w, h, ix + 1, iy + 1, c);
 }
 
 static void resizeArgbToYoloImg(char *imgSrc, float *imgDst, uint32_t wSrc,
-    uint32_t hSrc, uint32_t wDst, uint32_t hDst, float wRatio, float hRatio)
+    uint32_t hSrc, uint32_t wDst, uint32_t hDst, float wRatio, float hRatio,
+    bool interpolate)
 {
   for (uint32_t c = 0; c < 3; ++c) {
     for (uint32_t j = 0; j < hDst; ++j) {
       for (uint32_t i = 0; i < wDst; ++i) {
-        float v = bilinearInterpolationArgb(imgSrc, wSrc, hSrc, i * wRatio, 
-            j * hRatio, 2 - c);
+        float v;
+        if (interpolate) {
+          v = bilinearInterpolationArgb(imgSrc, wSrc, hSrc, i * wRatio, 
+              j * hRatio, 2 - c);
+        } else {
+          v = getPixelExtendArgb(imgSrc, wSrc, hSrc, 
+            static_cast<uint32_t>(i * wRatio), 
+            static_cast<uint32_t>(j * hRatio), 2 - c);
+        }
         imgDst[c * wDst * hDst + j * wDst + i] = v / 255.0f;
       }
     }
+  }
+}
+
+static void drawBoxArgb(char *img, uint32_t width, uint32_t i0, uint32_t j0, 
+    uint32_t w, uint32_t h, uint8_t r, uint8_t g, uint8_t b)
+{
+  uint32_t i1 = i0 + w;
+  uint32_t j1 = j0 + h;
+  for (uint32_t i = i0; i <= i1; ++i) {
+    img[(i + j0 * width) * 4 + 2] = r;
+    img[(i + j1 * width) * 4 + 2] = r;
+
+    img[(i + j0 * width) * 4 + 1] = g;
+    img[(i + j1 * width) * 4 + 1] = g;
+
+    img[(i + j0 * width) * 4] = b;
+    img[(i + j1 * width) * 4] = b;
+  }
+  for (uint32_t j = j0; j <= j1; ++j) {
+    img[(i0 + j * width) * 4 + 2] = r;
+    img[(i1 + j * width) * 4 + 2] = r;
+
+    img[(i0 + j * width) * 4 + 1] = g;
+    img[(i1 + j * width) * 4 + 1] = g;
+
+    img[(i0 + j * width) * 4] = b;
+    img[(i1 + j * width) * 4] = b;
   }
 }
 
@@ -79,15 +113,15 @@ int32_t main(int32_t argc, char **argv) {
     std::cerr << argv[0] << " detects and classifies objects in a camera feed "
       << "by using Yolo." << std::endl;
     std::cerr << "Usage:   " << argv[0] << " [--verbose]" << std::endl;
-    std::cerr << "         --cid: OD4 session conference id" << std::endl;
-    std::cerr << "         --cfg-file: Yolo configuration file" << std::endl;
-    std::cerr << "         --weight-file: Yolo weight file" << std::endl;
-    std::cerr << "         --name: name of the shared memory for the ARGB and XYZ"
+    std::cerr << "     --cid: OD4 session conference id" << std::endl;
+    std::cerr << "     --cfg-file: Yolo configuration file" << std::endl;
+    std::cerr << "     --weight-file: Yolo weight file" << std::endl;
+    std::cerr << "     --name: name of the shared memory for the ARGB and XYZ"
       << "formatted image (default: video0)" << std::endl;
-    std::cerr << "         --width: the width of the images " << std::endl;
-    std::cerr << "         --width: the height of the images " << std::endl;
-    std::cerr << "         --id: sender id of output messages" << std::endl;
-    std::cerr << "         --verbose: prints diagnostics data to screen" 
+    std::cerr << "     --width: the width of the images " << std::endl;
+    std::cerr << "     --height: the height of the images " << std::endl;
+    std::cerr << "     --id: sender id of output messages" << std::endl;
+    std::cerr << "     --verbose: prints diagnostics data to screen" 
       << std::endl;
     std::cerr << "Example: " << argv[0] << " --cfg-file=yolo.cfg "
       << "--weight-file=yolo.weight --width=1280 --height=720 [--name=video0] "
@@ -105,32 +139,12 @@ int32_t main(int32_t argc, char **argv) {
     uint32_t const id{(commandlineArguments["id"].size() != 0) ?
       static_cast<uint32_t>(std::stoi(commandlineArguments["height"])) : 0};
     bool const verbose{commandlineArguments.count("verbose") != 0};
-
-   /* char argbImage[] = {
-      0, static_cast<char>(-88), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-      0, 0, 0, 0, 0, static_cast<char>(-88), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-      0, 0, 0, 0, 0, 0, 0, 0, 0, static_cast<char>(-88), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, static_cast<char>(-88), 0, 0, 0, 0, 0, 0,
-      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, static_cast<char>(-88), 0, 0
-    };
-    float yoloImage[27];
-
-    resizeArgbToYoloImg(argbImage, yoloImage, 5, 5,
-        3, 3, 5.0f/3.0f, 5.0f/3.0f);
-
-    for (uint32_t n = 0; n < 3; ++n) {
-      for (uint32_t m = 0; m < 3; ++m) {
-        std::cout << yoloImage[n*m + n*3 + m] << " ";
-      }
-      std::cout << std::endl;
-    }
-
-
-    if (verbose) {
-      return 0;
-    }
-    */
-
+    
+    Display* display{nullptr};
+    Visual* visual{nullptr};
+    Window window{0};
+    XImage* ximage{nullptr};
+    
     Detector detector(commandlineArguments["cfg-file"],
         commandlineArguments["weight-file"]);
 
@@ -143,7 +157,8 @@ int32_t main(int32_t argc, char **argv) {
         << " bytes)." << std::endl;
     
     std::cout << "Connecting to shared memory " << nameXyz << std::endl;
-      std::unique_ptr<cluon::SharedMemory> shmXyz{new cluon::SharedMemory{nameXyz}};
+      std::unique_ptr<cluon::SharedMemory> shmXyz{
+        new cluon::SharedMemory{nameXyz}};
       if (shmXyz && shmXyz->valid()) {
         std::clog << argv[0] << ": Attached to shared depth memory '" 
           << shmXyz->name() << " (" << shmXyz->size() 
@@ -154,27 +169,46 @@ int32_t main(int32_t argc, char **argv) {
       yoloImg.w = detector.get_net_width();
       yoloImg.h = detector.get_net_height();
       yoloImg.c = 3;
-      yoloImg.data = new float[yoloImg.w * yoloImg.h * 3];
+      yoloImg.data = new float[yoloImg.w * yoloImg.h * yoloImg.c];
+
+      char *verboseImg{nullptr};
+      if (verbose) {
+        verboseImg = new char[width * height * 4];
+      }
+
+      if (verbose) {
+        display = XOpenDisplay(NULL);
+        visual = DefaultVisual(display, 0);
+        window = XCreateSimpleWindow(display, RootWindow(display, 0), 0, 0,
+            width, height, 1, 0, 0);
+        shmArgb->lock();
+        {
+          ximage = XCreateImage(display, visual, 24, ZPixmap, 0, verboseImg,
+              width, height, 32, 0);
+        }
+        shmArgb->unlock();
+      
+        XMapWindow(display, window);
+      }
     
-      float const widthRatio = static_cast<float>(width / yoloImg.w);
-      float const heightRatio = static_cast<float>(height / yoloImg.h);
+      float const widthRatio = static_cast<float>(width) / yoloImg.w;
+      float const heightRatio = static_cast<float>(height) / yoloImg.h;
 
       cluon::OD4Session od4{static_cast<uint16_t>(
           std::stoi(commandlineArguments["cid"]))};
 
       while (od4.isRunning()) {
+        cluon::data::TimeStamp t0 = cluon::time::now();
         shmArgb->wait();
         shmArgb->lock();
         
-        {
-          resizeArgbToYoloImg(shmArgb->data(), yoloImg.data, width, height,
-              yoloImg.w, yoloImg.h, widthRatio, heightRatio);
-        }
-        shmArgb->unlock();
+        resizeArgbToYoloImg(shmArgb->data(), yoloImg.data, width, height,
+            yoloImg.w, yoloImg.h, widthRatio, heightRatio, false);
 
         if (verbose) {
-          std::cout << "Detecting" << std::endl;
+          memcpy(verboseImg, shmArgb->data(), shmArgb->size());
         }
+        shmArgb->unlock();
 
         std::vector<bbox_t> detections = detector.detect(yoloImg, 0.2f, true);
 
@@ -184,9 +218,9 @@ int32_t main(int32_t argc, char **argv) {
           detection.y = static_cast<uint32_t>(heightRatio * detection.y);
           detection.h = static_cast<uint32_t>(heightRatio * detection.h);
         }
-        
-        detections = detector.tracking_id(detections, true, 5, 40);
 
+        detections = detector.tracking_id(detections, true, 5, 40);
+        
         shmXyz->wait();
         shmXyz->lock();
         {
@@ -205,10 +239,13 @@ int32_t main(int32_t argc, char **argv) {
           }
         }
         shmXyz->unlock();
-    
-        //   Output data
+
         if (verbose) {
-          std::cout << "Found objects: " << detections.size() << std::endl;
+          float fps = 1000000.0f / 
+              (cluon::time::toMicroseconds(cluon::time::now())
+                - cluon::time::toMicroseconds(t0));
+          std::cout << "Frames per second: " << fps << ", found objects "
+            << detections.size() << std::endl;
           for (auto &detection : detections) {
             std::cout << "  ... i=" << detection.x << ", j=" 
               << detection.y << ", w=" << detection.w << ", h=" << detection.h 
@@ -217,11 +254,18 @@ int32_t main(int32_t argc, char **argv) {
               << detection.frames_counter << ", x=" << detection.x_3d 
               << ", y=" << detection.y_3d << ", z=" << detection.z_3d 
               << std::endl;
+
+            drawBoxArgb(verboseImg, width, detection.x, detection.y,
+                detection.w, detection.h, 255, 0, 0);
           }
+
+          XPutImage(display, window, DefaultGC(display, 0), ximage, 0, 0, 0, 0,
+              yoloImg.w, yoloImg.h);
         }
       }
 
       delete[] yoloImg.data;
+      delete[] verboseImg;
     }
 
     retCode = 0;
